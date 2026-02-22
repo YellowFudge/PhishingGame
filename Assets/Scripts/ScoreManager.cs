b9a4a279ac721bc402d5ef7847a0895dff0b9331
Ikguzzi <ikguzzi@hotmail.com>
2026-02-22 16:02:32 +0100

    Added functionality to score system
    ScoreManager.cs now stores the true toggles in the checkbox

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,8 +1,41 @@
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
 public class ScoreManager : MonoBehaviour
 {
-  public void getScore() {
+    [SerializeField] private Toggle[] checklistOptions;
+    private List<Toggle> toggles = new List<Toggle>();
+    
+    public List<Toggle> OnCheckboxPressed()
+    {
+        toggles.Clear();
+        foreach (Toggle toggle in checklistOptions)
+        {
+            if (toggle.isOn)
+            {
+                toggles.Add(toggle);
+                Debug.Log("Toggled: " + toggle.name);
+            }
+        }
+        Debug.Log("Toggles total: " + toggles.Count);
+        foreach (var check in toggles)
+        {
+            Debug.Log(check.name);
+        }
 
-  }
+        return toggles;
+    }
+
+    public void OnFinalSendPressed()
+    {
+        List<Toggle> score = OnCheckboxPressed();
+        Debug.Log("Wizard sent" + score.Count);
+    }
+
+    public void OnFinalRejectPressed()
+    {
+        List<Toggle> score = OnCheckboxPressed();
+        Debug.Log("Wizard Rejected" + score.Count);
+    }
 }
