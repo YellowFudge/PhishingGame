181df3407469992b1fc682489cd17d2ca8cda61e
YellowFudge <113107666+YellowFudge@users.noreply.github.com>
2026-02-25 15:37:16 +0100

    First version of mail prefab
    also updated MailCueTypes (MailQueTypes before)

---
+++ Assets/Scripts/CustomScrollRect.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CustomScrollRect : ScrollRect
+{
+    private int _activePointerId = -1;
+
+    public override void OnScroll(PointerEventData data)
+    {
+        base.OnScroll(data);
+        velocity = Vector2.zero;
+    }
+
+
+    //-------------- Drag input tweaking -----------------
+    public override void OnInitializePotentialDrag(PointerEventData eventData)
+    {
+        //setting latest touch as current decider of drag
+        _activePointerId = eventData.pointerId;
+        base.OnInitializePotentialDrag(eventData);
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        //unsetting decider of drag if is current. Real pointerIDs will never be negative
+        if (eventData.pointerId == _activePointerId)
+        {
+            base.OnEndDrag(eventData);
+            _activePointerId = -1;
+        }
+            
+    }
+
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        //Only dragging if correct dragger
+        if(eventData.pointerId == _activePointerId)
+        {
+            base.OnBeginDrag(eventData);
+        }
+        
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        //Only dragging if correct dragger
+        if (eventData.pointerId == _activePointerId)
+        {
+            base.OnDrag(eventData);
+        }
+        
+    }
+
+
+}
---
+++ Assets/Scripts/MailCueTypes.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class MailCueTypes : MonoBehaviour
+{
+    [SerializeField] bool isSpamMail;
+    [SerializeField, Tooltip("The cue types of the mail")] CueTypes[] cueTypeArray;
+
+    public bool IsSpamMail { get {  return isSpamMail; } }
+    public CueTypes[] CueTypeArray { get { return cueTypeArray; } }
+
+
+    private void OnEnable()
+    {
+        if(isSpamMail && cueTypeArray.Length.Equals(0))
+        {
+            Debug.LogError($"The mail {this.gameObject.name} is marked as spam but has no cue types. Assign cue types.");
+        }
+        if (!isSpamMail && cueTypeArray.Length > 0)
+        {
+            Debug.LogWarning($"The mail {this.gameObject.name} is not marked as spam but has cue types. Set isSpamMail to true if this is not intentional.");
+        }
+    }
+
+    /// <summary>
+    /// Compares the internal cue types of a spam mail to the cue types in cueTypesToCompare
+    /// </summary>
+    /// <param name="cueTypesToCompare">the array of cuetypes to compare to the internal</param>
+    /// <param name="numOfMatches">number of matches found (if is spam)</param>
+    /// <returns>true if at least one match was found, false if no matches were found or isn't spam</returns>
+    public bool CompareCueTypes(CueTypes[] cueTypesToCompare, out int numOfMatches)
+    {
+        numOfMatches = 0;
+
+        if (!isSpamMail)
+        {
+            return false;
+        }
+
+        foreach(CueTypes compareType in cueTypesToCompare)
+        {
+            foreach(CueTypes internalType in cueTypeArray)
+            {
+                if(internalType == compareType)
+                {
+                    numOfMatches++;
+                    continue;
+                }
+            }
+        }
+
+        if (numOfMatches.Equals(0))
+        {
+            return false;
+        }
+        return true;
+    }
+
+}
+
+public enum CueTypes
+{
+    Error,
+    SenderDomain,
+    TooGoodToBeTrue,
+    GenericGreeting,
+    LogoImitiation,
+    NoBranding,
+    URLOrAttachment,
+    RequestInfo,
+    Urgency
+}
