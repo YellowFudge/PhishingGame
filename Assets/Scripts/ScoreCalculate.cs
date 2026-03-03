ce16a9198a201fa187476361b8e38069e35e7ff6
RichardBNilsson <rendjy@hotmail.com>
2026-03-03 13:53:54 +0100

    add functions to ScoreCalculate
    +CalculateQue can now check if correct/incorrect
    
    +CompareAnswer have math for levels
    
    +Changed variables
    
    +Changed comments
    
    +Cleaned up code

--- Assets/Scripts/ScoreCalculate.cs
+++ Assets/Scripts/ScoreCalculate.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -41,63 +42,85 @@ using UnityEngine.UI;
 
 public class ScoreCalculate : MonoBehaviour
 {
+
+
     //Player Variables
-    //Public PlayerEmailType playerEmailType; //Ham or Phising
+    //Public PlayerEmailType playerEmailType; // Ham or Phising
     //Public PlayerEmailQue playerEmailQue; // eg; 2 3 4
 
     //Email Variables
-    //Public EmailType emailType; //Ham or Phising
-    //Public EmailQue emailQue; //eg; 1 3 5
+    //Public EmailType emailType; // Ham or Phising
+    //Public EmailQue emailQue; // eg; 1 3 5
+
+    // Score variables
+    private const int scoreCorrect = 100; //Read up on Readonly vs const - which is better in this case
+    private const int scoreQueMax = 100; // --depening on method, might remove
+    public int score = 0; // Change variable name to more fitting - somthing related to active score for this mail
+    public int scoreQue = 0;
 
     //Other Variables
     public Button button;
 
     //Temporary variables
-    public string variableA = "Ham"; //Placeholder for Player email type
-    public string variableB = "Ham"; //Placeholder for Email Type
+    public string variableA = "Ham"; // Placeholder for Player email type
+    public string variableB = "Ham"; // Placeholder for Email Type
     public int[] pQue = {1, 0, 1, 0};
     public int[] eQue = {1, 1, 0, 0};
-    public int score = 0;
+    public int level = 1;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start() {
         button.onClick.AddListener(CompareAnswer);
-        Debug.Log("Voidstart");
     }
     
-    //Unsubscribe to prevent strange behavious (according to person on the internet, confirm w head of unity)
+    // Unsubscribe to prevent strange behavious (according to person on the internet, confirm w head of unity)
+    // TEST LATER
     private void OnDestroy() {
         button.onClick.RemoveListener(CompareAnswer);
-        Debug.Log("VoidDestroy");
     }
     
-    public void CompareAnswer () {
-        Debug.Log("Initiate CompareAnswer");
+    private void CompareAnswer () {
         if (variableA == variableB) {
-            Debug.Log("A and B are equal");
-            //Call god
-            //Add points to score
+            if (level == 1) {
+                score = scoreCorrect;
+            }
+            else if (level > 1) {
+                score = (scoreCorrect + (scoreCorrect / 2)) * (level - 1); // Fix math so it is 100 base and if statement not required?
+            }
+            else { Debug.Log("Error: Level not found"); }
+
+            Debug.Log("Score before quecalc : " + score);
+
             CalculateCue();
+
+            //Debug.Log("current score: " + score);
+            // Call script that sends the values
         }
         else {
             Debug.Log("A and B are diffrent");
-            //End script
+            // End script
         }
     }
 
-    public void CalculateCue() {
-        //Check playerEmailQue against emailQue
+    private void CalculateCue() {
+        // Check playerEmailQue against emailQue
         Debug.Log("Initilize Calculate Cue");
 
         for (int i = 0; i < eQue.Length; i++) {
-            Debug.Log("Player ques: " + pQue[i]);
-            Console.WriteLine(pQue[i]);
-            Debug.Log("Email ques: " + eQue[i]);
-            Console.WriteLine(eQue[i]);
+            //Debug.Log("Player ques: " + pQue[i]);
+            //Debug.Log("Email ques: " + eQue[i]);
 
-            //Calculate
-            //  Max point possible divided by Cue siez = Cue points per correct
-            //  Make rewards more rewarding 
+            if (pQue[i] == eQue[i]) {
+                Debug.Log("Equal");
+            }
+            else {
+                Debug.Log("Incorrect");
+            }
+
+            // Calculate
+            // Max point possible divided by Cue siez = Cue points per correct
+            // Make rewards more rewarding 
         }
     }
 
