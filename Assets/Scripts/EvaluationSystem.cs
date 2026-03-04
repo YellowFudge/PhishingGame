8891376501e7b23deaeb9383676de2a2a393ea53
RichardBNilsson <rendjy@hotmail.com>
2026-03-04 12:20:23 +0100

    Base logic for EvaluationSystem
    +Added temporary variables
    +Added function CheckCorrectTotal()
    +Added function CheckCorrectQue()

--- Assets/Scripts/EvaluationSystem.cs
+++ Assets/Scripts/EvaluationSystem.cs
@@ -5,11 +5,64 @@ public class EvaluationSystem : MonoBehaviour
 {
     public Button button;
 
+    /* Temporary variables */
+    public int totalCorrectMail = 8;
+    public int totalMail = 8;
+    public int totalPlayerQuePoints = 2000;
+    public int totalQuePoints = 2400;
+
     private void Start() {
         button.onClick.AddListener(CompareAnswer);
     }
 
     private void OnDestroy() {
         button.onClick.RemoveListener(CompareAnswer);
+    }
+
+    //  Change to %, do math
+
+    private void CheckCorrectTotal() {
+        //Great Success
+        if (totalCorrectMail == 7 || 8) {
+
+        }
+
+        //Minor Success
+        else if (totalCorrectMail == 5 || 6) {
+
+        }
+
+        //Minor Failure
+        else if (totalCorrectMail == 3 || 4) {
+
+        }
+
+        //Great Failure
+        else if (totalCorrectMail <= 2) {
+
+        }
+
+        else { Debug.Log("Something went wrong"); }
+
+        CheckCorrectQue();
+    }
+
+    private void CheckCorrectQue() {
+        //Great Success
+        if () {
+            //76%+
+        }
+
+        else if () {
+            //50 -> 75%
+        }
+
+        else if () {
+            //25 -> 49%
+        }
+
+        else if () {
+            //0 -> 24%
+        }
     }
 }
