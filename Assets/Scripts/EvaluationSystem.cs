6b2ee6bbdf51149877dc5a0930f4fd7942b2222c
RichardBNilsson <rendjy@hotmail.com>
2026-03-04 14:34:00 +0100

    Updated Evaluation CheckCorrectQue()
    +Added if statements for checking points based on percent
    +Added comments
    - minus 24 variable removed, caused dead zone between 24 and 25

--- Assets/Scripts/EvaluationSystem.cs
+++ Assets/Scripts/EvaluationSystem.cs
@@ -11,19 +11,19 @@ public class EvaluationSystem : MonoBehaviour
     public int totalPlayerQuePoints = 2000;
     public int totalQuePoints = 2400;
 
-    /*private void Start() {
-        button.onClick.AddListener(CompareAnswer);
+    private void Start() {
+        button.onClick.AddListener(CheckCorrectTotal);
     }
 
     private void OnDestroy() {
-        button.onClick.RemoveListener(CompareAnswer);
-    }*/
+        button.onClick.RemoveListener(CheckCorrectTotal);
+    }
 
     //  Change to %, do math
 
-    private void CheckCorrectTotal() {/*
+    private void CheckCorrectTotal() {
         //Great Success
-        if (totalCorrectMail == 7 || 8) {
+        /*if (totalCorrectMail == 7 || 8) {
 
         }
 
@@ -42,27 +42,38 @@ public class EvaluationSystem : MonoBehaviour
 
         }
 
-        else { Debug.Log("Something went wrong"); } */
+        else { Debug.Log("Something went wrong"); }*/
 
         CheckCorrectQue();
     }
 
     private void CheckCorrectQue() {
-        //Great Success
-        /*if () {
-            //76%+
+        double greatSuccess = totalQuePoints * 0.75;
+        double minorSuccess = totalQuePoints * 0.50;
+        double minorFailure = totalQuePoints * 0.25;
+
+        Debug.Log("greatSuccess: " + greatSuccess + "+ minorSuccess: " + minorSuccess + "+ minorFailure: " + minorFailure + "+ majorFailure: <" + minorFailure);
+
+        //Great Success 75% +
+        if (totalPlayerQuePoints >= greatSuccess)
+        { //75%+
+            Debug.Log("75%+ Great Success: " + totalPlayerQuePoints + " points out of: " + totalQuePoints);
         }
 
-        else if () {
-            //50 -> 75%
+        else if (totalPlayerQuePoints >= minorSuccess || totalPlayerQuePoints < greatSuccess)
+        { //50 -> 75%
+            Debug.Log("50% -> <75% Minor Success: " + totalPlayerQuePoints + " points out of: " + totalQuePoints);
         }
 
-        else if () {
-            //25 -> 49%
+        else if (totalPlayerQuePoints >= minorFailure || totalPlayerQuePoints < minorSuccess)
+        { //25 -> 49%
+            Debug.Log("25% -> <50% Minor Failure: " + totalPlayerQuePoints + " points out of: " + totalQuePoints);
         }
 
-        else if () {
-            //0 -> 24%
-        }*/
+        else if (totalPlayerQuePoints < minorFailure)
+        { //less than 25%-
+            Debug.Log("<25% -> 0% Great Failure: " + totalPlayerQuePoints + " points out of: " + totalQuePoints);
+        }
+        else { Debug.Log("Something went horrible wrong"); }
     }
 }
