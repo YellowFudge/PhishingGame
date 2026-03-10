359756b219751d840093bce18b8793ef29eab623
RichardBNilsson <rendjy@hotmail.com>
2026-03-10 15:08:36 +0100

    FIX ALL

--- Assets/Scripts/ScoreCalculate.cs
+++ Assets/Scripts/ScoreCalculate.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEditor.Experimental.Rendering;
 using UnityEngine;
@@ -46,72 +47,50 @@ public class ScoreCalculate : MonoBehaviour
 {
     public ScoreScriptableObjectScript scoreScriptableObjectScript;
 
-    //Player Variables
-    //Public PlayerEmailType playerEmailType; // Ham or Phising
-    //Public PlayerEmailQue playerEmailQue; // eg; 2 3 4
-
-    //Email Variables
-    //Public EmailType emailType; // Ham or Phising
-    //Public EmailQue emailQue; // eg; 1 3 5
-
     // Score variables
     private const int scoreCorrect = 100; //Read up on Readonly vs const - which is better in this case
     private const int scoreQueMax = 100; // --depening on method, might remove
-    public int score = 0; // Change variable name to more fitting - somthing related to active score for this mail
-    public int scoreQue = 0;
+    public float score = 0; // Change variable name to more fitting - somthing related to active score for this 
 
     //Other Variables
     private Button button;
     private DynamicButtons dynamicButtons;
-
-    //Temporary variables
-    public string variableA = "Ham"; // Placeholder for Player email type
-    public string variableB = "Ham"; // Placeholder for Email Type
-    public List<int> pQue = new List<int>();
-    public int[] eQue = {1, 1, 0, 0};
-    public int level = 1;
-    public int correctQue;
-    public int incorrectQue;
+    
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start() {
-        
-        button.onClick.AddListener(StartCalculation);
-        
+        //button.onClick.AddListener(StartCalculation);
     }
     
     // Unsubscribe to prevent strange behavious (according to person on the internet, confirm w head of unity)
     // TEST LATER
     private void OnDestroy() {
-        button.onClick.RemoveListener(CompareAnswer);
+        //button.onClick.RemoveListener(StartCalculation);
     }
 
-    public void StartCalculation() {
-        //Fetch parameters
-        dynamicButtons = GetComponent<DynamicButtons>();
-        dynamicButtons.GetResult();
-        pQue = dynamicButtons.GetResult();
-        Debug.Log(pQue);
+    public void StartCalculation(string playerMailType, string realMailType, List<int> emailQue, List<int> playerQue) {
 
         //Start calculation
-        CompareAnswer();
+        CompareAnswer(playerMailType, realMailType, emailQue, playerQue);
     }
     
-    private void CompareAnswer () {
-        if (variableA == variableB) {
-            if (level == 1) {
-                score = scoreCorrect;
-            }
-            else if (level > 1) {
-                score = (scoreCorrect + (scoreCorrect / 2)) * (level - 1); // Fix math so it is 100 base and if statement not required?
-            }
-            else { Debug.Log("Error: Level not found"); }
+    private void CompareAnswer (string playerMailType, string realMailType, List<int> emailQue, List<int> playerQue) {
+        if (playerMailType == realMailType) {
+            /* if (level == 1) {
+                 score = scoreCorrect;
+             }
+             else if (level > 1) {
+                 score = (scoreCorrect + (scoreCorrect / 2)) * (level - 1); // Fix math so it is 100 base and if statement not required?
+             }
+             else { Debug.Log("Error: Level not found"); } */
+
+            score = scoreCorrect;
 
             Debug.Log("Score before quecalc : " + score);
 
-            CalculateCue();
+            CalculateCue(emailQue, playerQue);
             // Call script that sends the values
         }
         else {
@@ -120,24 +99,23 @@ public class ScoreCalculate : MonoBehaviour
         }
     }
 
-    private void CalculateCue() {
+    private float CalculateCue(List<int> emailQue, List<int> playerQue) {
         // Check playerEmailQue against emailQue
         Debug.Log("Initilize Calculate Cue");
+        float scoreQue = 0;
+        int correctQue = 0;
 
-        for (int i = 0; i < eQue.Length; i++) {
+        for (int i = 0; i < emailQue.Count; i++) {
             //Fix according to enum which is correct and not correct
-            if (pQue[i] == eQue[i]) {
+            if (playerQue[i] == emailQue[i]) {
                 Debug.Log("Equal");
                 correctQue++;
             }
-            else {
-                Debug.Log("Incorrect");
-                incorrectQue++;
-            }
         }
 
-        scoreQue = (scoreQueMax / eQue.Length) * correctQue;
+        scoreQue = (scoreQueMax / emailQue.Count) * correctQue;
         Debug.Log(scoreQue);
+        return scoreQue;
     }
 
     public void God() {
