6764bcfaa6f3ba342270c130aa3177b8a3473618
YellowFudge <113107666+YellowFudge@users.noreply.github.com>
2026-03-10 13:57:14 +0100

    tweaked open/close handler
    current solution is ugly + needs improvements but better than before

--- Assets/Scripts/OpenCloseHandler.cs
+++ Assets/Scripts/OpenCloseHandler.cs
@@ -30,8 +30,69 @@ public class OpenCloseHandler : MonoBehaviour, IPointerClickHandler, /*ISubmitHa
         //checking position under parent
         if (!(_rectTrans.parent.childCount - 1).Equals(_rectTrans.GetSiblingIndex()))
         {
-            _rectTrans.SetAsLastSibling(); //making in front of everything under the same parent
-            return;
+            //check if is overlapped by other UI object
+            Vector3[] cornersA = new Vector3[4];
+
+            RectTransform rectA = null;
+
+            //find your own active child
+            for (int i = 0; i < _rectTrans.childCount; i++)
+            {
+                if (_rectTrans.parent.GetChild(i).gameObject.activeSelf)
+                    {
+                        rectA = _rectTrans.parent.GetChild(i).GetComponent<RectTransform>();
+                        break;
+                    }
+                
+            }
+            if (rectA == null)
+            {
+                rectA = _rectTrans; //return instead?
+            }
+
+            for(int i = 0; i < _rectTrans.parent.childCount; i++)
+            {
+                if (i.Equals(rectA.GetSiblingIndex()) || !_rectTrans.parent.GetChild(i).gameObject.activeSelf)
+                {
+                    continue;
+                }
+
+                RectTransform rectB = null;
+
+                if (_rectTrans.parent.GetChild(i).childCount > 1)
+                {
+                    for( int j = 0; j < _rectTrans.parent.GetChild(i).childCount; j++)
+                    {
+                        Debug.Log(_rectTrans.parent.GetChild(i).GetChild(j).gameObject.name);
+                        if (_rectTrans.parent.GetChild(i).GetChild(j).gameObject.activeSelf)
+                        {
+                            rectB = _rectTrans.parent.GetChild(i).GetChild(j).GetComponent<RectTransform>();
+                            break;
+                        }
+                    }
+                    if(rectB == null)
+                    {
+                        return;
+                    }
+
+                    Debug.Log(rectB);
+                    
+                }
+                else
+                {
+                    rectB = _rectTrans.parent.GetChild(i).GetComponent<RectTransform>();
+                }
+
+                     
+
+                if (CheckOverlap(cornersA, rectA, rectB))//needs to be moved to the front before you can interact with it
+                {
+                    _rectTrans.SetAsLastSibling(); //making in front of everything under the same parent
+                    return;
+                }
+            }
+            _rectTrans.SetAsLastSibling(); //already "at front" from player's prespective, so they want to interact with it as well == no return
+
         }
         
 
@@ -76,5 +137,25 @@ public class OpenCloseHandler : MonoBehaviour, IPointerClickHandler, /*ISubmitHa
     public void OnEndDrag(PointerEventData eventData)
     {
         _dragging = false;
+    }
+    bool CheckOverlap(Vector3[] ownCorners, RectTransform ownRect, RectTransform otherRect)
+    {
+        //check if is overlapped by other UI object
+        Vector3[] cornersB = new Vector3[4];
+
+        ownRect.GetWorldCorners(ownCorners);
+        otherRect.GetWorldCorners(cornersB);
+
+        Rect rect1 = new Rect(ownCorners[0], ownCorners[2] - ownCorners[0]);
+        Rect rect2 = new Rect(cornersB[0], cornersB[2] - cornersB[0]);
+
+        if (rect1.Overlaps(rect2))//needs to be moved to the front before you can interact with it
+        { 
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
