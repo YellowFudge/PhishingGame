290382290efdd496abcbd666c75b658b980c75ec
YellowFudge <113107666+YellowFudge@users.noreply.github.com>
2026-04-28 18:26:29 +0200

    Animations + new graphics

--- Assets/Scripts/MailStampManager.cs
+++ Assets/Scripts/MailStampManager.cs
@@ -6,16 +6,23 @@ public class MailStampManager : MonoBehaviour
     [SerializeField] MailStamp phishStamp;
     [SerializeField] MailStamp hamStamp;
     [SerializeField] GameObject stampCover;
+    Animator _animator;
     bool _hasStamped;
     bool _stampedAsPhishing;
 
     public bool HasStamped { get { return _hasStamped; }}
     public bool StampedAsPhishing { get { return _stampedAsPhishing; }}
 
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     private void OnEnable()
     {
         MailEventManager.ScrollOpenedEvent += OnScrollOpened;
-        MailEventManager.ScrollSentEvent += ResetStamps;
+        MailEventManager.ScrollSentEvent += ResetStamps;//make subscribe to when begins closing (sending)
+                                                        //so cannot extract stamps while it is closing to stamp next
     }
 
     private void OnDisable()
@@ -29,7 +36,8 @@ public class MailStampManager : MonoBehaviour
         //Remove cover (if not already opened)
         if (stampCover.activeSelf)
         {
-            stampCover.SetActive(false);
+            _animator.SetTrigger("Open");
+            //stampCover.SetActive(false);
         }
     }
 
@@ -43,7 +51,10 @@ public class MailStampManager : MonoBehaviour
     public void ResetStamps()
     {
         //cover with lid again
-        stampCover.SetActive(true);
+        //stampCover.SetActive(true);
+        _animator.SetTrigger("Close");
         _hasStamped = false;
     }
+
+    
 }
