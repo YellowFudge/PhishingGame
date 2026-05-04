b7f89670b9be001cb2b47b249e2454041873aff0
YellowFudge <113107666+YellowFudge@users.noreply.github.com>
2026-05-04 15:10:04 +0200

    Scrollchute + LinktextParent in mails

--- Assets/Scripts/ScrollChute.cs
+++ Assets/Scripts/ScrollChute.cs
@@ -175,7 +175,7 @@ public class ScrollChute : MonoBehaviour, IPointerClickHandler
         MailCueTypes mailCue = levelManager.GetCurrentMailinfo();
         
         //UNCOMMENT BELOW AND IT SHOULD WORK !!!! :)
-        //scoreCalculate.StartCalculation(isSpam, mailCue.IsSpamMail, dailyCues.dailyCuesArray, dynamicButtons.ConvertEnumToList().ToArray(), levelManager.CurrentDay);
+        //scoreCalculate.StartCalculation(isSpam, mailCue.IsSpamMail, mailCue.CueTypeArray, dynamicButtons.ConvertEnumToList().ToArray(), levelManager.CurrentDay);
         levelManager.NextMail(); //calling for next mail
     }
 }
